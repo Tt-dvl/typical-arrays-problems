b0ad04f4299cba0c0a379e42795f475252f9c2b7
Tetiana Kalko <taniakalko@gmail.com>
2020-10-03 15:35:02 +0300

    init:start arrays task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minimum = 0;
+  let arrayLength = array.length;
+
+  if (arrayLength > 0){
+
+  function compare(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1; 
+  }
+
+  array.sort(compare);
+
+  minimum = array[0];
+
+  return minimum;
+  }else 
+  
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum = 0;
+  let arrayLength = array.length;
+  
+  if (arrayLength > 0){
+  
+  function compare(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1; 
+  }
+  
+  array.sort(compare);
+  
+  for (let i = 0; i < arrayLength; i++) {
+    maximum = array[i];
+  }
+  
+  return maximum;
+  }else 
+  
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let average = 0;
+  let arrayLength = array.length;
+
+  if (arrayLength > 0){
+
+  let result = array.reduce((sum, current) => sum + current, 0); 
+
+  average = result/arrayLength;
+
+  return average;
+  }else 
+
+  return average;
 }
